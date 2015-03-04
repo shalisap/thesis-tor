7e5f0b72b05c6fddaa64da02823782cd3f839a4a
Shalisa Pattarawuttiwong <spattarawutt@gmail.com>
2015-03-04 09:06:46 -0500

    Cleaned up stray commented out code in clientlogging.c

--- src/or/clientlogging.c
+++ src/or/clientlogging.c
@@ -1,6 +1,6 @@
 /*  
  *  Shalisa Pattarawuttiwong 
- *  Last Modified: 08/04/2014
+ *  Last Modified: 03/04/2015
  */
 
 #include "address.h"
@@ -40,55 +40,25 @@ void cllog_log_cell(circuit_t *circ, cell_t *cell,
 			 
 	char *arrow;
 	char *log_command;
-//	uint32_t p_circ_id;
-//	uint32_t n_circ_id;
-	/* Get the IP addresses of the previous channel that sent and next channel
-	 * that will receive the cell we are logging.
-	 * Of course, this has to be eliminated later.
-	 */
-/*
-	tor_addr_t n_chan_addr;
-	tor_addr_t p_chan_addr;
-	char n_addr_s[TOR_ADDR_BUF_LEN] ;
-	char p_addr_s[TOR_ADDR_BUF_LEN] ; 
-	
-	channel_get_addr_if_possible(TO_OR_CIRCUIT(circ)->p_chan, &p_chan_addr);
-	channel_get_addr_if_possible(circ->n_chan, &n_chan_addr);
-
-	tor_addr_to_str(p_addr_s, &p_chan_addr, TOR_ADDR_BUF_LEN, 0) ;
-	tor_addr_to_str(n_addr_s, &n_chan_addr, TOR_ADDR_BUF_LEN, 0);
-*/	
 	
 	/* Determine which direction the cell is going in
-	 * and find the previous and next circuit ids.
+	 * and find the pseudonymized circuit id.
 	 */
 	if (cell_direction == CELL_DIRECTION_OUT) {
-		arrow = "->";
-	//	p_circ_id = cell->circ_id;
-	//	n_circ_id = circ->n_circ_id;
-				
-    } else if (cell_direction == CELL_DIRECTION_IN) {
+		arrow = "->"; 
+	} else if (cell_direction == CELL_DIRECTION_IN) {
 		arrow = "<-" ;
-	//	p_circ_id = TO_OR_CIRCUIT(circ)->p_circ_id;
-	//	n_circ_id = cell->circ_id;
-    } 
-	
+   	} 
+
+	/* Determine if it is a destroy, relay, or create cell
+ 	 */ 	
 	if (command == CELL_DESTROY) {
 		log_command = "DESTROY" ;
-    } else if (command == CELL_RELAY) {
+    	} else if (command == CELL_RELAY) {
 		log_command = "RELAY" ;
 	} else if (command == CELL_CREATE) {
 		log_command = "CREATE" ;
 	}
-
-/*
-	// For now, don't log the real circ_ids 
-	log_notice(LD_CLIENTLOGGING,
-			"CLIENTLOGGING: %s %s %s %s CIRC %" PRIx64 "",
-			log_command, p_addr_s, arrow, n_addr_s, circ->cllog_circ_id) ;
-		//	p_circ_id, arrow, n_circ_id,
-		//	circ->cllog_circ_id) ;
-  */
 
 	log_notice(LD_CLIENTLOGGING,
 			"CLIENTLOGGING: %s %"PRIx64 " %s %"PRIx64 " CIRC %"PRIx64 "",
